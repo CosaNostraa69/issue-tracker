e1f2abd9e9b26cfb0315dd0b929c7ec30aeddc49
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-06-12 11:23:08 +0200

    Update NavBar component to highlight current page and add classNames dependency

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,9 +1,14 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { SiBugsnag } from "react-icons/si";
+import classNames from 'classnames';
 
 
 const NavBar = () => {
+const currentPath = usePathname();
+
 const links = [
     { href: '/', label: 'Dashboard' },
     { href: '/issues', label: 'Issues' },
@@ -15,7 +20,15 @@ const links = [
         <SiBugsnag />
     </Link>
     <ul className='flex space-x-6'>
-        {links.map(link => <Link key={link.href}className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link> )}
+        {links.map(link => 
+        <Link 
+        key={link.href}
+        className={classNames({
+            'text-zinc-900': link.href === currentPath,
+            'text-zinc-500': link.href !== currentPath,
+            'hover:text-zinc-800 transition-colors': true
+        })} 
+        href={link.href}>{link.label}</Link> )}
         
     </ul>
 
